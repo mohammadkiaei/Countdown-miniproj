d65b6d3e07b3acb8ca9f0ed753598614beee3042
Mohammad Kiaei <78914731+mohammadkiaei@users.noreply.github.com>
2022-08-12 01:33:50 +0430

    ata

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('aug 20, 2022 00:00:00').getTime();
+    const countDate = new Date('8 20, 2022 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now ;
 
