1fb6505ebe8cf9fb86ad57954c66c63d59718469
Mohammad Kiaei <78914731+mohammadkiaei@users.noreply.github.com>
2023-04-09 12:50:36 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('Oct 24, 2022 00:00:00').getTime();
+    const countDate = new Date('Oct 24, 2023 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now ;
 
