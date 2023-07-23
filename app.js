faf9911b06007b6e78e9b36b28e542a8dd710b05
Mohammad Kiaei <78914731+mohammadkiaei@users.noreply.github.com>
2023-07-24 02:56:44 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const countdown = () => {
-    const countDate = new Date('Oct 24, 2023 00:00:00').getTime();
-    const now = new Date().getTime();
-    const gap = countDate - now ;
+   const countDate = new Date('Oct 24, 2023 00:00:00').getTime();
+   const now = new Date().getTime();
+   const gap = countDate - now;
 
-    // How Time Does Work?
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+   // How Time Does Work?
+   const second = 1000;
+   const minute = second * 60;
+   const hour = minute * 60;
+   const day = hour * 24;
 
-    //Calculate the Time
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor ((gap % day) / hour);
-    const textMinute = Math.floor ((gap % hour) / minute);
-    const textSecond = Math.floor((gap % minute) / second);
+   //Calculate the Time
+   const textDay = Math.floor(gap / day);
+   const textHour = Math.floor((gap % day) / hour);
+   const textMinute = Math.floor((gap % hour) / minute);
+   const textSecond = Math.floor((gap % minute) / second);
 
-    document.querySelector(".day").innerText = textDay;
-    document.querySelector(".hour").innerText = textHour;
-    document.querySelector(".minute").innerText = textMinute;
-    document.querySelector(".second").innerText = textSecond;
+   document.querySelector('.day').innerText = textDay;
+   document.querySelector('.hour').innerText = textHour;
+   document.querySelector('.minute').innerText = textMinute;
+   document.querySelector('.second').innerText = textSecond;
 
-    console.log(gap);
+   console.log(gap);
 };
-
 
 setInterval(countdown, 1000);
 
-// Developed by SeyedMohammadMahdi Kiaei
+// Developed by Mohammad Kiaei
